aa9819b4302d9cb097455c336946d5026a65b2a3
abenoit <abenoit@student.42.fr>
2020-09-11 17:04:03 +0200

    clean color ratio

--- bonus/src/sfx.c
+++ bonus/src/sfx.c
@@ -6,7 +6,7 @@
 /*   By: abenoit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 13:09:48 by abenoit           #+#    #+#             */
-/*   Updated: 2020/09/11 16:18:37 by abenoit          ###   ########.fr       */
+/*   Updated: 2020/09/11 17:02:55 by abenoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,19 @@
 #include "ft_utils.h"
 #include "cub_macro.h"
 #include "cub_struct.h"
+
+static int	apply_ratio_to_channel(int color_input, int ratio,
+									int color_mod, int channel)
+{
+	int			color;
+
+	color = ((int)(((1.0 - ratio)
+						* (double)(color_input & channel))
+						+ (ratio
+						* (double)(color_mod & channel)))
+						& channel);
+	return (color);
+}
 
 int			apply_fog(double dist, int color_input, t_param *prm)
 {
@@ -30,9 +43,9 @@ int			apply_fog(double dist, int color_input, t_param *prm)
 		if (fog_percent > 1.0)
 			fog_percent = 1.0;
 		color = 0;
-		color += ((int)(((1.0 - fog_percent) * (double)(color_input & 0x000000FF)) + (fog_percent * (double)(conf->fog_color & 0x000000FF))) & 0x000000FF);
-		color += ((int)(((1.0 - fog_percent) * (double)(color_input & 0x0000FF00)) + (fog_percent * (double)(conf->fog_color & 0x0000FF00))) & 0x0000FF00);
-		color += ((int)(((1.0 - fog_percent) * (double)(color_input & 0x00FF0000)) + (fog_percent * (double)(conf->fog_color & 0x00FF0000))) & 0x00FF0000);
+		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x000000FF);
+		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x0000FF00);
+		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x00FF0000);
 	}
 	return (color);
 }
--- bonus/src/sky.c
+++ bonus/src/sky.c
@@ -6,7 +6,7 @@
 /*   By: abenoit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/30 18:06:48 by abenoit           #+#    #+#             */
-/*   Updated: 2020/09/08 16:45:21 by abenoit          ###   ########.fr       */
+/*   Updated: 2020/09/11 16:47:42 by abenoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,30 @@
 
 void	fill_sky_line(int x, t_ray *ray, t_param *prm)
 {
-	int		sky_x;
-	int		sky_y;
-	double	Rx;
-	double	Ry;
-	int		y;
-	double	plop;
-	double	CIRCLE = 3.14159265359;
-	t_tx	*sky;
-	t_screen	*screen;
-	int			pos_y;
+	t_coord			ratio;
+	t_int_coord		sky_coord;
+	int				y;
+	double			plop;
+	t_tx			*sky;
+	t_screen		*screen;
+	int				offset_y;
 
 	sky = get_lst_elem(prm->dlist, ID_TX_SK)->content;
 	screen = get_lst_elem(prm->dlist, ID_RES)->content;
 	y = 0;
-	Rx = ((double)sky->width / 8.0) / ((double)screen->width);
-	plop = (atan2(ray->dir.y, ray->dir.x) / CIRCLE) / 2;
+	ratio.x = ((double)sky->width / 8.0) / ((double)screen->width);
+	plop = (atan2(ray->dir.y, ray->dir.x) / M_PI) / 2;
 	if (plop < 0)
 		plop = 1.0 + plop;
-	Ry = ((double)sky->height / 2.0) / ((double)screen->height);
-	sky_x = (int)(floor((x * Rx) + (plop * sky->width)));
+	ratio.y = ((double)sky->height / 2.0) / ((double)screen->height);
+	sky_coord.x = (int)(floor((x * ratio.x) + (plop * sky->width)));
 	while (y < ray->horizon)
 	{
-		pos_y = ((screen->height * 2.0) - (ray->horizon - y)); 
-		sky_y = (int)floor((double)pos_y * Ry);
-		if (sky_y > 0 && sky_y < sky->height - 1)
+		offset_y = ((screen->height * 2.0) - (ray->horizon - y));
+		sky_coord.y = (int)floor((double)offset_y * ratio.y);
+		if (sky_coord.y > 0 && sky_coord.y < sky->height - 1)
 		{
-			ray->color = get_pixel_color(sky_x, sky_y, &sky->data);
+			ray->color = get_pixel_color(sky_coord.x, sky_coord.y, &sky->data);
 			if ((ray->color & 0x00FFFFFF) != 0)
 				ray->line_buff[y] = ray->color;
 		}
