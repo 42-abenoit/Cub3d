2cbdef5bc26e05d2a7fb5ff97ad717794a94e349
abenoit <abenoit@student.42.fr>
2020-09-12 12:17:13 +0200

    https://framagit.org/abenoit/cub_parser.git

--- bonus/src/sfx.c
+++ bonus/src/sfx.c
@@ -6,7 +6,7 @@
 /*   By: abenoit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 13:09:48 by abenoit           #+#    #+#             */
-/*   Updated: 2020/09/12 11:58:20 by abenoit          ###   ########.fr       */
+/*   Updated: 2020/09/12 12:14:41 by abenoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,9 +42,12 @@ int			apply_fog(double dist, int color_input, t_param *prm)
 		if (fog_percent > 1.0)
 			fog_percent = 1.0;
 		color = 0;
-		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x000000FF) & 0x000000FF;
-		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x0000FF00) & 0x0000FF00;
-		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x00FF0000) & 0x00FF0000;
+		color += apply_ratio_to_channel(color_input, fog_percent,
+				conf->fog_color, 0x000000FF) & 0x000000FF;
+		color += apply_ratio_to_channel(color_input, fog_percent,
+				conf->fog_color, 0x0000FF00) & 0x0000FF00;
+		color += apply_ratio_to_channel(color_input, fog_percent,
+				conf->fog_color, 0x00FF0000) & 0x00FF0000;
 	}
 	return (color);
 }
@@ -52,14 +55,21 @@ int			apply_fog(double dist, int color_input, t_param *prm)
 int			apply_hit_effect(int color_input)
 {
 	int			color;
+	int			sfx_color;
+	double		sfx_ratio;
 
 	color = 0;
+	sfx_color = 0x00FFFF00;
+	sfx_ratio = 0.5;
 	if ((color_input & 0x00FFFFFF) != 0)
 	{
 		color = 0;
-		color += ((int)((0.5  * (double)(color_input & 0x000000FF)) + (0.5 * (double)(0x00FFFF00 & 0x000000FF))) & 0x000000FF);
-		color += ((int)((0.5 * (double)(color_input & 0x0000FF00)) + (0.5 * (double)(0x00FFFF00 & 0x0000FF00))) & 0x0000FF00);
-		color += ((int)((0.5 * (double)(color_input & 0x00FF0000)) + (0.5 * (double)(0x00FFFF00 & 0x00FF0000))) & 0x00FF0000);
+		color += apply_ratio_to_channel(color_input, sfx_ratio,
+				sfx_color, 0x000000FF) & 0x000000FF;
+		color += apply_ratio_to_channel(color_input, sfx_ratio,
+				sfx_color, 0x0000FF00) & 0x0000FF00;
+		color += apply_ratio_to_channel(color_input, sfx_ratio,
+				sfx_color, 0x00FF0000) & 0x00FF0000;
 	}
 	return (color);
 }
