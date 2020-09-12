2a5bb40307ecd0c1067d53d297de12c4377ce2e6
abenoit <abenoit@student.42.fr>
2020-09-12 12:07:03 +0200

    fog ratio int cast fix

--- bonus/src/sfx.c
+++ bonus/src/sfx.c
@@ -6,7 +6,7 @@
 /*   By: abenoit <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/08 13:09:48 by abenoit           #+#    #+#             */
-/*   Updated: 2020/09/11 17:02:55 by abenoit          ###   ########.fr       */
+/*   Updated: 2020/09/12 11:58:20 by abenoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include "cub_macro.h"
 #include "cub_struct.h"
 
-static int	apply_ratio_to_channel(int color_input, int ratio,
+static int	apply_ratio_to_channel(int color_input, double ratio,
 									int color_mod, int channel)
 {
 	int			color;
@@ -24,8 +24,7 @@ static int	apply_ratio_to_channel(int color_input, int ratio,
 	color = ((int)(((1.0 - ratio)
 						* (double)(color_input & channel))
 						+ (ratio
-						* (double)(color_mod & channel)))
-						& channel);
+						* (double)(color_mod & channel))));
 	return (color);
 }
 
@@ -43,9 +42,9 @@ int			apply_fog(double dist, int color_input, t_param *prm)
 		if (fog_percent > 1.0)
 			fog_percent = 1.0;
 		color = 0;
-		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x000000FF);
-		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x0000FF00);
-		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x00FF0000);
+		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x000000FF) & 0x000000FF;
+		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x0000FF00) & 0x0000FF00;
+		color += apply_ratio_to_channel(color_input, fog_percent, conf->fog_color, 0x00FF0000) & 0x00FF0000;
 	}
 	return (color);
 }
